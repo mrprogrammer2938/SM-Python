99c243ac5a7c7870a3ca5af4f084149a55bf81b0
Mr.programmer <78996423+mrprogrammer2938@users.noreply.github.com>
2021-06-23 07:05:19 -0400

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+# This code write by Mr.nope
+clear
+echo "Installing..."
+chmod a+x SM_python.py
+sudo apt install python
+sudo apt install python3
+sudo cp SM_python.py /usr/lib/python3.6
+sudo cp SM_python.py /usr/lib/python3
+echo ""
+echo "Installing..., Finish...!"
+echo ""
+exit 1
